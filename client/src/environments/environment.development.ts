38820eece5c27d22edf396be75572ef73e193dba
mina <peemlomsak@gmail.com>
2023-11-13 17:57:15 +0700

    final fs

---
+++ client/src/environments/environment.development.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+    production: false,
+    apiUrl: "",
+    auth0_domain: "",
+    auth0_clientId: "",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+};
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+    production: true,
+    apiUrl: "",
+    auth0_domain: "",
+    auth0_clientId: "",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "",
+};
